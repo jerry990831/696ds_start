c20e1f4393ea7b2cc91a80a542e82d823c786475
Weiqi Wang <49157425+jerry990831@users.noreply.github.com>
2024-02-07 10:45:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import torch
+from transformers import LlamaTokenizer, LlamaForCausalLM
+import json
 
-def print_hi(name):
-    print(f'Hi, {name}')
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import login
+
+login("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")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+data_path = "dataset/train_rand_split.jsonl"
+dataset = []
+with open('dataset/train_rand_split.jsonl', 'r') as f:
+    for line in f:
+        data = json.loads(line)
+        dataset.append(data)
+
+
+def evaluate_question(question):
+    prompt = question['stem'] + "\n"
+    for choice in question['choices']:
+        prompt += f"{choice['label']}: {choice['text']}\n"
+    inputs = tokenizer(prompt, return_tensors="pt", padding=True)
+    outputs = model(**inputs)
+    print(outputs)
+    return outputs
+
+
+for item in dataset:
+    question = item['question']
+    answer_key = item['answerKey']
+    selected_answer = evaluate_question(question)
