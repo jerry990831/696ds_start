094c9407f209edd57b0e9bc7856001b5fc8c974b
Weiqi Wang <49157425+jerry990831@users.noreply.github.com>
2024-02-07 16:49:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ with open('dataset/train_rand_split.jsonl', 'r') as f:
         dataset.append(data)
         break
 
+
 def evaluate_question(question):
-    prompt = "choose an answer of this question with any reason \n"+question['stem'] + "\n"
+    prompt = "choose an answer of this question with any reason \n" + question['stem'] + "\n"
     for choice in question['choices']:
         prompt += f"{choice['label']}: {choice['text']}\n"
     inputs = tokenizer.encode(prompt, return_tensors='pt')
     output = model.generate(inputs, max_length=300, no_repeat_ngram_size=3,
-                            temperature=0.5)
+                            temperature=1)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     print("________________________________________")
     print(generated_text)
