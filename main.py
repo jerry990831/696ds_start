b068dcecf9a48f2c82ef58065e6b84f96ae1bad1
Weiqi Wang <49157425+jerry990831@users.noreply.github.com>
2024-02-07 16:41:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with open('dataset/train_rand_split.jsonl', 'r') as f:
     for line in f:
         data = json.loads(line)
         dataset.append(data)
-
+        break
 
 def evaluate_question(question):
     prompt = "choose an answer of this question with any reason \n"+question['stem'] + "\n"
@@ -27,7 +27,7 @@ def evaluate_question(question):
     return generated_text
 
 
-for item in dataset[10:]:
+for item in dataset:
     question = item['question']
     answer_key = item['answerKey']
     selected_answer = evaluate_question(question)
