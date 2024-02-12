a0b268965e2e28f47e1f452303fa2c961cb4b880
Weiqi Wang <49157425+jerry990831@users.noreply.github.com>
2024-02-12 14:56:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 login("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")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 data_path = "dataset/train_rand_split.jsonl"
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf").to(torch_device).half()
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf").to(torch_device)
 dataset = []
 with open('dataset/train_rand_split.jsonl', 'r') as f:
     for line in f:
@@ -21,6 +21,8 @@ def evaluate_question(question, decode_method="greedy"):
     prompt += question['stem'] + "\n"
     for choice in question['choices']:
         prompt += f"{choice['label']}: {choice['text']}\n"
+    print(prompt)
+    print("--------------------------")
     input_ids = tokenizer.encode(prompt, return_tensors='pt').to(torch_device)
     if decode_method == "greedy":
         output = model.generate(input_ids, max_length=200)
@@ -32,8 +34,5 @@ dataset = dataset[:100]
 for i in range(100):
     question1 = dataset[i]['question']
     answer_key1 = dataset[i]['answerKey']
-    print("Question" + str(i))
-    print("----------------------------------------------------")
     evaluate_question(question1)
-    print("----------------------------------------------------")
-    print(answer_key1)
+
